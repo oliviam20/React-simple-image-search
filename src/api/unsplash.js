8b9aa5c8d0f0c3b5e59efde3ab52f0867dc05b87
Olivia Mo <oliviatrademark@gmail.com>
2019-11-13 17:51:05 +1100

    update api key location

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import secret from '../secret.json';
 
 // api @ https://unsplash.com/documentation#location
 // auth @ https://unsplash.com/documentation#authorization
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: `Client-ID ${secret.unsplashApi.accessKey}`
+    Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
   }
 });
