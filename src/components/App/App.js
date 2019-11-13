1169f57c90b6928062ffcc4923cd887857313101
Olivia Mo <oliviatrademark@gmail.com>
2019-11-13 18:51:59 +1100

    add image list component and basic styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import unsplash from '../../api/unsplash';
 import SearchBar from '../SearchBar/SearchBar';
+import ImageList from '../ImageList/ImageList';
 import './App.css';
 
 class App extends Component {
@@ -21,6 +22,7 @@ class App extends Component {
       <div className="ui container appContainer">
         <SearchBar onSubmit={this.onSearchSubmit} />
         <p>{this.state.images.length} images found</p>
+        <ImageList images={this.state.images} />
       </div>
     )
   }
